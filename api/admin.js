04b796af986e8404982ce6c71e7b9fb9e50e7a63
Dr SHXWN <seanmandivhei47@gmail.com>
2026-01-08 22:17:13 +0200

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -1,1 +1,13 @@
+const ADMIN = 'admin@tlink.app';
 
+export default async function handler(req, res) {
+  if (req.headers['x-admin'] !== ADMIN) {
+    return res.status(403).end();
+  }
+
+  res.json({
+    users: 'ok',
+    apps: 'ok',
+    ledger: 'ok'
+  });
+}
